f5b9a05afc114657cafc3a20e4aa089c2a6afc78
Jack Evans <jackevans24@googlemail.com>
2021-12-11 12:58:03 +0000

    Added ability to join custom rooms Collapse loading and lobby scenes into one

--- Assets/Scripts/Networking/RoomManager.cs
+++ Assets/Scripts/Networking/RoomManager.cs
@@ -1,24 +1,87 @@
 using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private InputField createInput;
-    [SerializeField] private InputField joinInput;
+    [SerializeField] private CanvasGroup loadingGroup;
+    [SerializeField] private CanvasGroup joinRoomGroup;
+
+    [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private TMP_InputField createInput;
+    [SerializeField] private TMP_InputField joinInput;
+
+    [SerializeField] private TMP_Text errorText;
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        this.loadingGroup.alpha = 0;
+        this.joinRoomGroup.alpha = 1;
+        this.joinRoomGroup.interactable = true;
+    }
+
+    public override void OnLeftLobby()
+    {
+        this.joinRoomGroup.interactable = false;
+        this.joinRoomGroup.alpha = 0;
+        this.loadingGroup.alpha = 1;
+
+        PhotonNetwork.JoinLobby();
+    }
 
     public void CreateRoom()
     {
-        // TODO: Use input text
-        PhotonNetwork.CreateRoom("NewRoom");
+        if (string.IsNullOrWhiteSpace(this.createInput.text))
+        {
+            this.errorText.text = "Room code invalid";
+            return;
+        }
+
+        this.SetName();
+        PhotonNetwork.CreateRoom(this.createInput.text);
     }
 
     public void JoinRoom()
     {
-        // TODO: Use input text
-        PhotonNetwork.JoinRoom("NewRoom");
+        if (string.IsNullOrWhiteSpace(this.joinInput.text))
+        {
+            this.errorText.text = "Room code invalid";
+            return;
+        }
+
+        this.SetName();
+        PhotonNetwork.JoinRoom(this.joinInput.text);
+    }
+
+    private void SetName()
+    {
+        var name = this.nameInput.text;
+        if (string.IsNullOrWhiteSpace(name))
+            name = "Player";
+
+        PhotonNetwork.NickName = name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        this.errorText.text = "Create room failed: " + message;
+        base.OnCreateRoomFailed(returnCode, message);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        this.errorText.text = "Join room failed: " + message;
+        base.OnJoinRoomFailed(returnCode, message);
     }
 
     public override void OnJoinedRoom()
